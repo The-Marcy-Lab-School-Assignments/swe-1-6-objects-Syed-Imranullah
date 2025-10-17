706b490e9675a696ccc7fdba96d60918f77d5de9
Syed <xayanmay@gmail.com>
2025-10-16 20:40:19 -0400

    solved couple of them

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,10 +1,20 @@
-const coolGreeting = () => {
+const coolGreeting = (person) => {
+  if (person.isCool === true) {
+    return `What is UP ${person.name.toUpperCase()}? How you been doin'?`
+  } else {
+    return `Greetings ${person.name}, how have you been lately?`
+  }
+
 };
 
-const haveBirthday = () => {
+const haveBirthday = (person) => {
+  person.age = person.age + 1
 };
 
-const becomeSecretAgent = () => {
+const becomeSecretAgent = (person, spyHandle) => {
+  delete person.name
+  person.spyHandle = spyHandle
+  return person
 };
 
 const carMaker = () => {
